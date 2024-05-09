066227cbae57a995fab830c65aa82b3fbe2cc75e
MohsinMalikAWan <nomanmalik5320@gmail.com>
2024-05-09 18:44:23 +0500

    Update Files

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
-var result_num = document.getElementById('result_num')
-var user_num = document.getElementById('user_num')
-var result_box = document.getElementById('result_box')
+// var result_num = document.getElementById('result_num')
+// var user_num = document.getElementById('user_num')
+// var result_box = document.getElementById('result_box')
 
-function checkNumber(userNumber) {
-    // alert(userNumber)
+// function checkNumber(userNumber) {
+//     var randomNumber = Math.ceil(Math.random() * 6)
+//     user_num.innerText = userNumber
+//     result_num.innerText = randomNumber
+//     if (userNumber == randomNumber) {
+//         result_box.innerText = 'Congrats!, You won'
+//         result_box.style.color = 'green'
+//     } else {
+//         result_box.innerText = 'Soory!, You lose , Try Again'
+//         result_box.style.color = 'red'
+//     }
+// }
 
-    var randomNumber = Math.ceil(Math.random() * 6)
-    user_num.innerText = userNumber
-    result_num.innerText = randomNumber
-    if (userNumber == randomNumber) {
-        result_box.innerText = 'Congrats!, You won'
-        result_box.style.color = 'green'
-    } else {
-        result_box.innerText = 'Soory!, You lose , Try Again'
-        result_box.style.color = 'red'
-    }
+
+var value = new Date
+// console.log(value.getDate()+ "/" + value.getMonth()+ "/" + value.getFullYear())
+
+console.log(value.getTime())
+console.log(Math.floor(value /1000))
+console.log(Math.floor(value /1000 / 60))
+console.log(Math.floor(value /1000 / 60 /60))
+console.log(Math.floor(value /1000 / 60 /60 /24))
+console.log(Math.floor(value /1000 / 60 /60/24/30))
+console.log(Math.floor(value /1000 / 60 /60/24/30/12))
+
+
+
+var hours = document.getElementById('hours')
+var minuts = document.getElementById('minuts')
+var sec = document.getElementById('sec')
+var stopTime = document.getElementById('click')
+
+
+
+
+
+var updateInterval = setInterval(function(){
+    var date = new Date
+      hours.innerText = date.getHours()
+      minuts.innerText = date.getMinutes()
+      sec.innerText = date.getSeconds()
+},1000)
+
+function StopTime(){
+  clearInterval(updateInterval)
 }
-
-
-var value = gat.time
-console.log(value)
